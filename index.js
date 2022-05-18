d75029dbfc569af343ee9abad0f1c81464a20d9e
maddy404 <97498608+maddy404@users.noreply.github.com>
2022-05-18 08:55:30 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,5 @@
+function toggle(id) {
+  var element = document.getElementById(id);
+  if (element.style.display == "none") element.style.display = "block";
+  else element.style.display = "none";
+}
